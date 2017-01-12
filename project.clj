b8b6dc4bd8042b9cf004b3a32b4514283376e064
vbedegi <vbedegi@gmail.com>
2017-01-12 21:57:54 +0100

    :url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject re-alm "0.1.0"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/vbedegi/re-alm"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
