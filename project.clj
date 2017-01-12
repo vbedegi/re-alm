da69938d2a89319063c61069cc42238782d75595
vbedegi <vbedegi@gmail.com>
2017-01-12 21:34:50 +0100

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-alm "0.1.0-SNAPSHOT"
+(defproject re-alm "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
