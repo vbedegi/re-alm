8bac6e1731787546f6b0ba83be4c896565a84c90
vbedegi <vbedegi@gmail.com>
2017-03-26 17:23:38 +0200

    updating deps

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/vbedegi/re-alm"
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [org.clojure/core.async "0.2.395"]
-                 [reagent "0.6.0"]
+                 [org.clojure/clojurescript "1.9.495"]
+                 [org.clojure/core.async "0.3.442"]
+                 [reagent "0.6.1"]
                  [re-frame "0.7.0"]
                  [cljs-ajax "0.5.8"]
-                 [jarohen/chord "0.7.0"]
+                 [jarohen/chord "0.8.1"]
                  [alandipert/storage-atom "2.0.1"]]
 
   :plugins [[lein-cljsbuild "1.1.4"]]
