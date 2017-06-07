faa3cf2d53c95cfc82a0b2d0192dfa110f531e28
vbedegi <vbedegi@gmail.com>
2017-06-07 18:52:40 +0200

    going 0.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 # Change Log
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
-## [0.2.2-SNAPSHOT]
+## [0.2.2]
 ### Added
 - Eventbus - publishing and listening to messages, through topics
 - DataStore - reading and writing to a global datastore, with notifications
--- examples/eventbus/project.clj
+++ examples/eventbus/project.clj
@@ -11,7 +11,7 @@
                  [org.clojure/core.async  "0.3.443"
                   :exclusions [org.clojure/tools.reader]]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [re-alm "0.2.2-SNAPSHOT"]]
+                 [re-alm "0.2.2"]]
 
   :plugins [[lein-figwheel "0.5.10"]
             [lein-cljsbuild "1.1.5" :exclusions [[org.clojure/clojure]]]]
